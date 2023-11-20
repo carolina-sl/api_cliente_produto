637962eeec7b9603cf45847afb6d6a069d32d079
Carolina Serafim Lauffer <carolina.lauffer@gmail.com>
2023-11-19 23:54:38 -0300

    alterada a consulta do metodo index do controller Pedidos.

--- app/Http/Controllers/api/PedidosController.php
+++ app/Http/Controllers/api/PedidosController.php
@@ -13,10 +13,8 @@ use App\Models\Produto;
 class PedidosController extends Controller {
 
     public function index() {
-        
+
         $pedidos = Pedido::join('clientes', 'pedidos.cliente_id', '=', 'clientes.id')
-        ->join('pedido_items', 'pedido_items.pedido_id', '=', 'pedidos.id')
-        ->join('produtos', 'pedido_items.produto_id', '=', 'produtos.id')
         ->select('pedidos.id as numero do pedido',
         'clientes.nome as nome do cliente',
         'clientes.cpf as cpf do cliente',
